73640073d10b191c3360f3b23c0f24cce81cd025
Mikhal Demchenko <dev.echo.mike@gmail.com>
2019-11-02 14:58:46 +0300

    [+]Added compiler barrier utility

---
+++ ecsl/utility/CompilerBarrier.hpp
@@ -0,0 +1,48 @@
+#ifndef ECSL_UTILITY_COMPILER_BARRIER_HPP_
+#define ECSL_UTILITY_COMPILER_BARRIER_HPP_
+
+/**
+ * @file CompilerBarrier.hpp
+ * Adds compiler instuction reorder barrier
+ */
+
+/// STD
+#include <atomic>
+
+namespace ecsl {
+
+/**
+ * @brief Prevents compiler from reordering of instructions higher and lower
+ * than call to this function during the optimizations.
+ * Usable with ecsl::prefetch functions
+ * Is same as ecsl::compiler_barrier and ecsl::privent_reorder
+ */
+inline void reorder_barrier()
+{
+    std::atomic_signal_fence(std::memory_order_seq_cst);
+}
+
+/**
+ * @brief Prevents compiler from reordering of instructions higher and lower
+ * than call to this function during the optimizations.
+ * Usable with ecsl::prefetch functions
+ * Is same as ecsl::reorder_barrier and ecsl::privent_reorder
+ */
+inline void compiler_barrier()
+{
+    std::atomic_signal_fence(std::memory_order_seq_cst);
+}
+
+/**
+ * @brief Prevents compiler from reordering of instructions higher and lower
+ * than call to this function during the optimizations.
+ * Usable with ecsl::prefetch functions
+ * Is same as ecsl::compiler_barrier and ecsl::reorder_barrier
+ */
+inline void privent_reorder()
+{
+    std::atomic_signal_fence(std::memory_order_seq_cst);
+}
+
+} // namespace ecsl
+#endif /* ECSL_UTILITY_COMPILER_BARRIER_HPP_ */
